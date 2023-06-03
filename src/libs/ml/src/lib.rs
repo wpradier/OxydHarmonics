069c44185bb1db384a397cf0647a1b0674ecf493
arys <charles.yaiche.lf@gmail.com>
2023-06-03 14:24:56 +0200

    work on interop

--- src/libs/ml/src/lib.rs
+++ src/libs/ml/src/lib.rs
@@ -1,35 +1,74 @@
 mod pmc_model;
+mod linear_model;
+
 use pmc_model::{create_pmc,train_pmc , PmcModel};
+use linear_model::{LinearRegressionModel};
 use std::slice;
+use ndarray::{Array, Array1, Array2};
+use rand::distributions::Uniform;
 use tensorboard_rs;
-struct LinearModel {
-
-}
 
 
 
 #[no_mangle]
-extern "C" fn create_linear_model() -> *mut LinearModel {
-    let model = Box::new(LinearModel {});
+extern "C" fn create_linear_model(len: i32) -> *mut LinearRegressionModel {
+    unsafe {
+        let model_size: i32 = len;
+        let layer_weight = Array::random(
+            (1, model_size + 1),
+            Uniform::new(-1.0, 1.0)
+        );
+        let model = Box::new(LinearRegressionModel { W: layer_weight });
 
-    Box::leak(model)
+        Box::leak(model)
+    }
 }
 
 
 #[no_mangle]
-extern "C" fn train_linear_model(model: *mut LinearModel, x_train: *const f64, lines: i32,
-                                 columns: i32, y_train: *const f64, y_train_columns: i32,
-                                    alpha: f64, epochs: i32) -> *const f64 {
-    const V: f64 = 1.0;
+extern "C" fn train_linear_model(model: *mut LinearRegressionModel,
+                                 x_train: *const f64, lines: i32, columns: i32,
+                                 y_train: *const f64, y_train_columns: i32,
+                                    alpha: f64, epochs: i32, is_classification: bool){
+   let mut linear_model : LinearRegressionModel =
+       unsafe {
+           model.read()
+       };
 
-    return &V;
+    let mut _x_train : Array2<f64> =
+        unsafe {
+            Array2::from_vec(Vec::from_raw_parts(x_train.cast_mut(),
+                                                 (columns * lines) as usize,
+                                                 (columns * lines) as usize))
+                .reshape((lines, columns))
+        };
+
+    let mut _y_train : Array2<f64> =
+        unsafe {
+            Array2::from_vec(Vec::from_raw_parts(y_train.cast_mut(),
+                                                 y_train_columns as usize,
+                                                 (y_train_columns + 1) as usize))
+                .reshape(1, y_train_columns)
+        };
+
+    linear_model._fit(_x_train, _y_train, epochs, alpha, is_classification);
 }
 
 #[no_mangle]
-extern "C" fn predict_linear_model(model: *mut LinearModel, sample_input: *const f64, lines: i32,
-                                    columns: i32) -> f64 {
-    const V: f64 = 0.0;
-    return V;
+extern "C" fn predict_linear_model(model: *mut LinearModel, sample_input: *const f64, lines: i32) -> f64 {
+     let mut linear_model : LinearRegressionModel =
+       unsafe {
+           model.read()
+       };
+
+    let mut x_input : Array1<f64> =
+        unsafe {
+            Array1::from_vec(Vec::from_raw_parts(sample_input.cast_mut(),
+                                                 (lines) as usize,
+                                                 (lines) as usize))
+        };
+
+    return linear_model.predict(x_input);
 }
 
 #[no_mangle]
---
+++ src/libs/ml/src/linear_model.rs
@@ -0,0 +1,67 @@
+use ndarray::{array, Array1, Array2, ArrayView, concatenate, Axis, Ix2, s, Array};
+
+pub struct LinearRegressionModel {
+    pub W : Array1<f64>
+}
+
+impl LinearRegressionModel {
+    pub fn _get_trained_variable(&self) -> &Array1<f64> {
+        &self.W
+    }
+
+    // Gradiant descent for multiple features
+    pub fn _fit(&mut self, X_train: Array2<f64>, Y_train: Array2<f64>, epoch: i32, alpha: f64, is_classification: bool) {
+        let m  = X_train.nrows(); // number of training example
+        let mf : f64 = m as f64; // number of training example as float 64
+        let bias = Array::<f64, Ix2>::from_elem((X_train.shape()[0], 1), 1.);
+
+        let X_train = concatenate![Axis(1), bias, X_train];
+
+        for _ in 0..epoch {
+            let mut Wbis = self.W.clone();
+            for j in 0..(self.W.shape()[0]) {
+                let mut w = Wbis[j];
+                let mut grad = 0.;
+                println!("j : {},\n{:?}", j, X_train);
+
+                for i in 0..m {
+                    //getting the training example features [i]
+                    let X_i = X_train.slice(s![i, ..]);
+
+                    //getting the training example output
+                    let Y_i = &Y_train.slice(s![i, ..]);
+
+                    //Hypotesis function : WᵗX
+                    let mut hyp = self.W.dot(&X_i);
+                    println!("«loop : {}..{} - {:?}\nxi: {:?}»",i, m ,  hyp, X_i);
+
+                    if is_classification {
+                        hyp = sig(&hyp);
+                    }
+
+                    let loss = hyp - Y_i;
+                    grad += loss[[0]] * X_i[j];
+
+                }
+                //should implement the cost function W = W - (α / n) Σ (WᵗX - Y) * X
+                // for each W at the same time
+                Wbis[j] = w - (alpha / mf) * grad;
+            }
+            // updating W with new values
+            self.W = Wbis;
+            println!("new weight : {}", self.W)
+        }
+    }
+
+
+    pub fn predict(&self, x_predict : Array1<f64>) -> f64 {
+        let bias : Array1<f64> = array![1.0];
+        let x = concatenate![Axis(0), bias, x_predict];
+
+        self.W.dot(&x)
+    }
+}
+
+fn sig(a: &f64) -> f64 {
+    1.0 / (1.0 + (-a).exp())
+}
