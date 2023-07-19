5c78726824da5e4ad47e7c4263897b94b2663cfe
William Pradier <xantowill@gmail.com>
2023-07-19 15:48:39 +0200

    Preparing for merge

--- modules/ml/ml_core/src/lib.rs
+++ modules/ml/ml_core/src/lib.rs
@@ -3,79 +3,14 @@ use std::ffi::{c_char, c_int, c_uint, CStr};
 use std::ops::Deref;
 use std::ptr::slice_from_raw_parts;
 use std::result;
-use crate::models::linear::{self, LinearRegressionModel};
-use crate::models::mlp;
-use crate::models::mlp::MultilayerPerceptron;
+use crate::models::mlp::{self, MultilayerPerceptron};
 
 
 mod models;
 mod utils;
 
-/*** LINEAR ***/
 
-#[no_mangle]
-extern "C" fn create_linear_model(len: i32) -> *mut LinearRegressionModel {
-    let usize_len = usize::try_from(len).unwrap();
-    let model = Box::new(linear::create(usize_len));
-
-    Box::leak(model)
-}
-
-
-#[no_mangle]
-extern "C" fn train_linear_model(model: *mut LinearRegressionModel,
-                                 x_train: *mut f64, lines: usize, columns: usize,
-                                 y_train: *mut f64, y_train_columns: usize,
-                                    alpha: f64, epochs: u32, is_classification: bool) {
-    unsafe {
-        let input_dataset = slice_from_raw_parts(x_train, lines * columns)
-            .as_ref().unwrap()
-            .chunks(columns)
-            .map(|x| x.to_vec())
-            .collect::<Vec<_>>();
-
-        let output_dataset = slice_from_raw_parts(y_train, y_train_columns).as_ref().unwrap().to_vec();
-
-        //println!("INPUT VEC: {:?}", input_dataset);
-        //println!("OUTPUT VEC: {:?}", output_dataset);
-
-
-        linear::train(model.as_mut().unwrap(), input_dataset, output_dataset, alpha, epochs, is_classification);
-    }
-}
-
-#[no_mangle]
-extern "C" fn predict_linear_model(model: *mut LinearRegressionModel, sample_input: *mut f64, columns: i32, is_classification: bool) -> f64 {
-    unsafe {
-        let input_vec = slice_from_raw_parts(sample_input, usize::try_from(columns).unwrap()).as_ref().unwrap().to_vec();
-
-        linear::predict(model.as_ref().unwrap(), &input_vec, is_classification)
-    }
-}
-
-#[no_mangle]
-extern "C" fn save_linear_model(model: *mut LinearRegressionModel, filename: *const c_char) {
-    unsafe {
-        let c_str = CStr::from_ptr(filename).to_str().unwrap();
-        //TODO SAVE
-    }
-}
-
-#[no_mangle]
-extern "C" fn destroy_linear_model(model: *mut LinearRegressionModel) {
-    unsafe {
-        dealloc(model as *mut u8, Layout::new::<LinearRegressionModel>());
-    }
-}
-
-#[no_mangle]
-extern "C" fn load_linear_model(filename: *const u8) -> *mut LinearRegressionModel {
-    let model = Box::new(linear::create(1));
-
-    Box::leak(model)
-}
-
-/*** MLP / MULTILAYER PERCEPTRON ***/
+/** MULTILAYER PERCEPTRON **/
 
 #[no_mangle]
 extern "C" fn create_mlp_model(structure: *mut usize, len: usize) -> *mut MultilayerPerceptron {
